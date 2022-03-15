15cdbf5a632f1ebe09f79cd7db1b62136dd05509
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 23:18:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,10 @@ token = '5189222022:AAHwMwbam7yvCOFoHlfzYcqpYOA3FOyW21o'
 bot = telebot.TeleBot(token)
 
 
-    
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text="Привет", reply_markup=markup)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+    bot.send_photo(message.chat.id, photo=open('img/q.jpg', 'rb'))
 
 if __name__ == '__main__':
      bot.infinity_polling()
