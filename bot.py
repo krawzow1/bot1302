3ca02210182f01121c0321ec47c0617bbc6dc198
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 23:01:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,12 @@ def repeat_all_messages(message):
     bot.send_message(message.chat.id, message.text)
     bot.send_photo(message.chat.id, photo=open('img/q.jpg', 'rb'))
     
-@bot.message_handler(commands=['start'])
-def keyboard():
-  start_keyboard = types.InlineKeyboardMarkup()
-  menu = types.InlineKeyboardButton(text='Наше меню', callback_data='menu')
-  start_keyboard.add(menu)
-  bot.send_message(message.chat.id, 'нажми на кнопку', reply_markup=start_keyboard)
+@bot.message_handler(commands=['start']) #создаем команду
+def start(message):
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("Сайт Хабр", url='https://habr.com/ru/all/')
+    markup.add(button1)
+    bot.send_message(message.chat.id, "Привет, {0.first_name}! Нажми на кнопку и перейди на сайт)".format(message.from_user), reply_markup=markup)kup)
 
 if __name__ == '__main__':
      bot.infinity_polling()
