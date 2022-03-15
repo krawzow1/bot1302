9cd823518c3eebac549ddb896e4461a261e58a65
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-16 00:33:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1=types.KeyboardButton("Кнопка")
     markup.add(item1)
-    bot.send_message(message.chat.id,'Выберите что вам надо',reply_markup=markup)
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}!",reply_markup=markup)
 
 if __name__ == '__main__':
      bot.infinity_polling()
