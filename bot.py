a8c85a8c3df7dcb1cec11c3d36f81f3062f91f95
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 23:05:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ def start(message):
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("Сайт Хабр", url='https://habr.com/ru/all/')
     markup.add(button1)
-    bot.send_message(message.chat.id, "Привет! Нажми на кнопку и перейди на сайт)".format(message.from_user), reply_markup=markup)kup)
 
 if __name__ == '__main__':
      bot.infinity_polling()
