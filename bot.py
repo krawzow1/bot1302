a88712bb92ac3d4b8ebf4c10cdd5f244c94a9dd8
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-16 00:16:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ bot = telebot.TeleBot(token)
 def start_message(message):
 	bot.send_message(message.chat.id,'Привет')	
        
+@bot.message_handler(commands=['button'])
+def button_message(message):
+	markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1=types.KeyboardButton("Кнопка")
+  markup.add(item1)	
 
 if __name__ == '__main__':
      bot.infinity_polling()
