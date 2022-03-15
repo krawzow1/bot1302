288fa0542cdb6ba4ca44293b84fac2a45577d899
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 23:17:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ def start(message):
     btn1 = types.KeyboardButton("👋 Поздороваться")
     btn2 = types.KeyboardButton("❓ Задать вопрос")
     markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(message.from_user), reply_markup=markup)
+    bot.send_message(message.chat.id, text="Привет", reply_markup=markup)
 
 if __name__ == '__main__':
      bot.infinity_polling()
