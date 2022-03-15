c98816ccdc3ca11439cf716ae1ab8bb87e36ebbd
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-16 00:14:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-  bot.send_message(message.chat.id,"Привет")
-
+	bot.send_message(message.chat.id,'Привет')
+	
 @bot.message_handler(commands=['button'])
 def button_message(message):
 	markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
