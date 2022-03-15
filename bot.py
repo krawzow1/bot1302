1c967386fc8574060dd9da2c202f79843ff55afb
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 23:15:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,7 @@ import telebot
 token = '5189222022:AAHwMwbam7yvCOFoHlfzYcqpYOA3FOyW21o'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-    bot.send_photo(message.chat.id, photo=open('img/q.jpg', 'rb'))
+
     
 @bot.message_handler(commands=['start'])
 def start(message):
