47a9bf4e21b3e02f6e480d4468abbc62e2cff0ff
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-15 09:11:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли
     bot.send_message(message.chat.id, message.text)
+    bot.send_photo(chat_id=chat_id, photo=open('tests/test.png', 'rb'))
 
 if __name__ == '__main__':
      bot.infinity_polling()
