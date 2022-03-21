1944f5bd6f66cd54aa5a62ded4e2f61ad9ebefb6
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-21 14:12:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,28 @@ from telebot import types # для указание типов
 token = '5189222022:AAHwMwbam7yvCOFoHlfzYcqpYOA3FOyW21o'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
-    item2=types.KeyboardButton("Кнопка2")
-    item3=types.KeyboardButton("Игра")
-    markup.add(item1, item2, item3)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот".format(message.from_user), reply_markup=markup)
-
-@bot.message_handler(content_types='text')
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")  
+    
+@bot.message_handler(commands=["button"])
+def button_message(message):
+    markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton("Войти в замок, я же не трус.")
+    item2 = types.KeyboardButton("Поступить разумно и дальше искать выход из леса.")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(message.chat.id, 'Как поступишь?', reply_markup = markup)      
+        
+@bot.message_handler(content_types=["text"])
 def message_reply(message):
-    if message.text=="Кнопка":
-        bot.send_photo(message.chat.id, photo=open('vze2.jpg', 'rb'))
-    if message.text=="Кнопка2":
-        bot.send_message(message.chat.id, "vk.com/goshkazavr")
-    if message.text=="Игра":
-        bot.send_message(message.chat.id, "Игра началась")
-        if message.text=="Игра началась":
-            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item10=types.KeyboardButton("dsfdsf")
-            item20=types.KeyboardButton("22222")
-            item30=types.KeyboardButton("213dfdsfds")
-            markup.add(item10, item20, item30)
-            bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот".format(message.from_user), reply_markup=markup) 
-  
-         
-
+    if message.text == "Войти в замок, я же не трус.":
+        markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
+        b1 = types.KeyboardButton("Белый и пушистый, с колбасой")
+        b2 = types.KeyboardButton("Темный и страшный, с ножницами в лапках")
+        markup.add(b1)
+        markup.add(b2)
+        bot.send_message(message.chat.id, 'Перед тобой две двери, на одной из них нарисован темный и страшный кот, который держит в руках ножницы, а на другой белый и пушистый котик, в руках которого колбаса. В какую дверь пойдешь?', reply_markup = markup)  
 
 if __name__ == '__main__':
      bot.infinity_polling()
