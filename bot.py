3894a33c2fe6a390e49413637b36d292d3210150
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-21 13:20:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,21 +18,13 @@ def button_message(message):
     markup.add(item1)
     markup.add(item2)
     bot.send_message(message.chat.id, 'Как поступишь?', reply_markup = markup)    
-
-@bot.message_handler(content_types=["text"])
-def message_reply(message):
-    if message.text == "Войти в замок, я же не трус.":
-         #bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)  
          
 
 @bot.message_handler(content_types=["text"])
 def message_reply(message):
     if message.text == "Поступить разумно и дальше искать выход из леса.":
         bot.send_message(message.chat.id, 'Удачи с этим.', reply_markup = markup)           
-    
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
      bot.infinity_polling()
