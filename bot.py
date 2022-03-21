dd6bb35b618977d39d3da5b9fcc92d535e2ec5ca
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-21 13:19:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,40 +6,33 @@ bot = telebot.TeleBot(token)
 
 
 
-#@bot.message_handler(commands=['img'])
-#def image(message):
-#    bot.send_photo(message.chat.id, photo=open('img/q.jpg', 'rb'))
-
 @bot.message_handler(commands=["start"])
 def start_message(message):
-    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")
+    bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")  
     
-@bot.message_handler(content_types=['text'])
-def func(message):
-    if(message.text == "Поздороваться"):
-        bot.send_message(message.chat.id, text="Привеет...")
-    elif(message.text == "Задать вопрос"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Как меня зовут?")
-        btn2 = types.KeyboardButton("Что я могу?")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+@bot.message_handler(commands=["button"])
+def button_message(message):
+    markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton("Войти в замок, я же не трус.")
+    item2 = types.KeyboardButton("Поступить разумно и дальше искать выход из леса.")
+    markup.add(item1)
+    markup.add(item2)
+    bot.send_message(message.chat.id, 'Как поступишь?', reply_markup = markup)    
+
+@bot.message_handler(content_types=["text"])
+def message_reply(message):
+    if message.text == "Войти в замок, я же не трус.":
+         #bot.send_message(message.chat.id, 'Я могу отправить мем с котиком.', reply_markup = markup)  
+         
+
+@bot.message_handler(content_types=["text"])
+def message_reply(message):
+    if message.text == "Поступить разумно и дальше искать выход из леса.":
+        bot.send_message(message.chat.id, 'Удачи с этим.', reply_markup = markup)           
     
-    elif(message.text == "Как меня зовут?"):
-        bot.send_message(message.chat.id, "У меня нет имени..")
-    
-    elif message.text == "Что я могу?":
-        bot.send_message(message.chat.id, text="Поздороваться с пользователем")
-    
-    elif (message.text == "Вернуться в главное меню"):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("Поздороваться")
-        button2 = types.KeyboardButton("Задать вопрос")
-        markup.add(button1, button2)
-        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
-    else:
-        bot.send_message(message.chat.id, text="На такую комманду я не запрограммировал..")
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
      bot.infinity_polling()
