36a18bd86abcd1368b651e1138beee14e1fb08df
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-23 10:07:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,35 +6,6 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=["start"])
 def start_message(message):
     bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")  
-    
-@bot.message_handler(commands=["button"])
-def button_message(message):
-    markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton("Войти в замок, я же не трус.")
-    item2 = types.KeyboardButton("Поступить разумно и дальше искать выход из леса.")
-    markup.add(item1)
-    markup.add(item2)
-    bot.send_message(message.chat.id, 'Как поступишь?', reply_markup = markup)      
-        
-@bot.message_handler(content_types=["text"])
-def message_reply(message):
-    if message.text == "Войти в замок, я же не трус.":
-        markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
-        b1 = types.KeyboardButton("Белый и пушистый, с колбасой")
-        b2 = types.KeyboardButton("Темный и страшный, с ножницами в лапках")
-        markup.add(b1)
-        markup.add(b2)
-        bot.send_message(message.chat.id, 'Перед тобой две двери, на одной из них нарисован темный и страшный кот, который держит в руках ножницы, а на другой белый и пушистый котик, в руках которого колбаса. В какую дверь пойдешь?', reply_markup = markup)  
-
-@bot.message_handler(content_types=["text"])
-def message_reply(message):
-    if message.text == "Белый и пушистый, с колбасой":
-        markup= types.ReplyKeyboardMarkup(resize_keyboard = True)
-        bu1 = types.KeyboardButton("Ключ")
-        bu2 = types.KeyboardButton("Колбасу")
-        markup.add(bu1)
-        markup.add(bu2)
-        bot.send_message(message.chat.id, 'Ты нравишься белому и пушистому котику и он предлагает на выбор: ключ и колбасу. Что выберешь?', reply_markup = markup) 
         
       
   if __name__ == '__main__':
