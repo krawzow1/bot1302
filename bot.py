27354c2c35f0249bc29a4644e31764c19da24a0e
krawzow1 <101524373+krawzow1@users.noreply.github.com>
2022-03-23 10:08:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types # для указание типов
 token = '5189222022:AAHwMwbam7yvCOFoHlfzYcqpYOA3FOyW21o'
 bot = telebot.TeleBot(token)
 
@@ -8,5 +7,5 @@ def start_message(message):
     bot.send_message(message.chat.id, text = "Ты заблудился ночью в лесу. Долго ходил-бродил и искал выход из леса, но ТУТ ты увидел жутко темный, жутко подозрительный и жутко жуткий замок.")  
         
       
-  if __name__ == '__main__':
-     bot.infinity_polling()
+if __name__ == '__main__':
+    bot.infinity_polling()
